c1a9cc9714c1aa1f4d72a42c26d621dcbc216dc2
John <johnevans096@gmail.com>
2024-02-21 10:56:54 -0500

    SCRUM-26 Add url for product

--- vests/urls.py
+++ vests/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from vests import views
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('product/', views.product, name='product')
 ]
