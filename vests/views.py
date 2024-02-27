7fcb6f7686e5eb205c2cee9d1515893b578e180f
NIK8H <nikithatwork@gmail.com>
2024-02-27 12:46:57 -0500

    SCRUM-35 add views and url for get product quantities feature

--- vests/views.py
+++ vests/views.py
@@ -7,3 +7,5 @@ def about(request):
     return render(request, 'about.html')
 def product(request):
     return render(request, 'product.html')
+def get_quantities(request):
+        # Query the database to get available quantities for the selected size
