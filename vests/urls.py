a6baf3291bfb9dd84a17d94bd3b34a1b84465660
John <johnevans096@gmail.com>
2024-03-03 13:57:33 -0500

    SCRUM-23, SCRUM-26 Modify existing urls and add url for cart page.

--- vests/urls.py
+++ vests/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from vests import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('product/', views.product, name='product')
+    path('', views.home, name='home'),  # url for home page
+    path('about/', views.about, name='about'),  # url for about page
+    path('product/', views.product, name='product'),  # url for product page
+    path('cart/', views.cart, name='cart'),  # url for cart page
 ]
