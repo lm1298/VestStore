97b2e1e4dcdc100288f092a4976efe21cd10e6dc
Vyshnavi Mulakalapalli <113145088+VyshnaviMulakalapalli@users.noreply.github.com>
2024-06-18 14:08:51 -0400

    Update settings.py

--- vest_store/settings.py
+++ vest_store/settings.py
@@ -148,12 +148,12 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # Set to True if you want sessions to e
 PAYPAL_RECEIVER_EMAIL = "vesttestBusiness@gmail.com"
 PAYPAL_TEST = True  # For sandbox mode
 PAYPAL_MODE = 'sandbox'  # sandbox or live
-PAYPAL_CLIENT_ID = 'AZPlVLeV8iUwn6RjA3Qk_0qGbjJkhloSxRaQO_EWYZilIXybr2uRENsrTIca2r6jxy-P4zsOHuahkvoR'
-PAYPAL_CLIENT_SECRET = 'EPBW5fK6c_8t8fZX9DVvE9Ul_clr4-TYZH10MSurPmgDBI_7OIOZnfxmaTzbqEF5LLVuQFJ50YUbbR0A'
+PAYPAL_CLIENT_ID = ''
+PAYPAL_CLIENT_SECRET = ''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '25mphveststore@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'ocggsmbshiimzsmc'  # Your Gmail password or app password if 2-step verification is enabled
+EMAIL_HOST_USER = ''  # Your Gmail address
+EMAIL_HOST_PASSWORD = ''  # Your Gmail password or app password if 2-step verification is enabled
 EMAIL_PORT = 587  # For TLS
 EMAIL_USE_TLS = True
